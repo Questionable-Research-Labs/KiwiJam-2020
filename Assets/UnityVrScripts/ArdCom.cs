bc5b21103905c97b169a2e48c900f7629e9cf1d5
Jasper M-W <jasper@ngita.co.nz>
2020-07-17 20:23:14 +1200

    Initial Unity VR Shell

---
+++ Assets/UnityVrScripts/ArdCom.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+namespace UnityVRScripts {
+
+    
+    public class ArdCom : MonoBehaviour {
+        // Start is called before the first frame update
+        public String arduinoPort;
+        public bool useArdunio;
+
+        public bool leftRelayStatus = false;
+        public bool rightRelayStatus = false;
+        
+        private SerialPort serialPortStream;
+
+        void Start() {
+            if (useArdunio)
+            {
+                serialPortStream = new SerialPort(arduinoPort, 9600);
+                serialPortStream.Open(); 
+            }
+            else {
+                Debug.Log("Ard is not enabled");
+            }
+
+            TurnAllRelaysOff();
+        }
+
+        public void SendToArduino (String message) {
+            
+            // // register the event
+            // port.DataReceived += Port_DataReceived;
+            //open the port
+            if (useArdunio) {
+                try {
+                    // start the communication
+                    Debug.Log("Sending message to Ard: " + message);
+                    serialPortStream.Write(message + ">");
+                }
+                catch (Exception ex) {
+                    Debug.Log("Writing failed! \nError: " + ex.Message);
+                }
+            }
+            else {
+                Debug.Log(message + " sent to arduino sim");
+            }
+        }
+
+        public void TurnAllRelaysOff() {
+            LeftRelayOff();
+            RightRelayOff();
+        }
+        public void TurnAllRelaysOn() {
+            LeftRelayOn();
+            RightRelayOn();
+        }
+        public void LeftRelayOn() {
+            leftRelayStatus = true;
+            SendToArduino("01");
+        }
+
+        public void LeftRelayOff() {
+            leftRelayStatus = false;
+            SendToArduino("00");
+        }
+        public void RightRelayOn() {
+            rightRelayStatus = true;
+            SendToArduino("11");
+        }
+
+        public void RightRelayOff() {
+            rightRelayStatus = false;
+            SendToArduino("10");
+        }
+    }
+}
---
+++ ardunioSerial/ardunioSerial.ino
@@ -0,0 +1,92 @@
+const byte numChars = 32;
+char receivedChars[numChars]; // an array to store the received data
+int lengthOfData;
+const int led = 2;
+boolean newData = false;
+const int relayPins[2] = {2,3};
+
+void setup() {  
+  Serial.begin(9600);
+  while(!Serial){}
+  pinMode(relayPins[0], OUTPUT);
+  pinMode(relayPins[1], OUTPUT);
+  Serial.println("Ardunio Serial is on");
+}
+
+void loop() {
+    recvWithEndMarker();
+    if (newData == true) {
+    Serial.print("Recived serial message: ");
+    Serial.println(receivedChars);
+    if (lengthOfData == 2) {
+      int relayPin = 0;
+      if (receivedChars[0] == '0') {
+        Serial.println("Left relay command");
+        relayPin = relayPins[0];
+      } else if (receivedChars[0] == '1') {
+        Serial.println("Right relay command");
+        relayPin = relayPins[1];
+      } else {
+        Serial.println("Unkonwn relay address");
+        return;
+      }
+      if (receivedChars[1] == '0') {
+        Serial.println("Turing Off pin " + String(relayPin));
+        digitalWrite(relayPin, HIGH);
+      } else if (receivedChars[1] == '1') {
+        Serial.println("Turing On pin " + String(relayPin));
+        digitalWrite(relayPin, LOW);
+      } else {
+        Serial.println("Unkonwn relay state");
+        return;
+      }
+    } else {
+        Serial.println("Message was not correct size: " + String(lengthOfData));
+    }
+     
+     newData = false;
+   }
+}
+
+String getNextSerialMessage() {
+  String readString;
+  String Q;
+  while(!Serial.available()){}
+  while (Serial.available()) {
+    delay(1);  //delay to allow buffer to fill 
+    if (Serial.available() >0) {
+      char c = Serial.read();  //gets one byte from serial buffer
+      if (isControl(c)) {
+        //'Serial.println("it's a control character");
+        break;
+      }
+      readString += c; //makes the string readString    
+     }
+  }
+  return readString;
+}
+void recvWithEndMarker() {
+ static byte ndx = 0;
+ lengthOfData = 0;
+ char endMarker = '>';
+ char rc;
+ 
+ // if (Serial.available() > 0) {
+ while (Serial.available() > 0 && newData == false) {
+   rc = Serial.read();
+  
+   if (rc != endMarker) {
+    receivedChars[ndx] = rc;
+    ndx++;
+    if (ndx >= numChars) {
+      ndx = numChars - 1;
+    }
+   }
+   else {
+    receivedChars[ndx] = '\0'; // terminate the string
+    lengthOfData = ndx;
+    ndx = 0;
+    newData = true;
+   }
+ }
+}

af8f301ae9f21c369f55555ecf0b75eab2b254d4
Jasper <jasper@ngita.co.nz>
2020-07-18 12:01:00 +1200

    Gun charges up

---
+++ Assets/UnityVrScripts/BulletManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityVRScripts {
+    public class BulletManager : MonoBehaviour {
+        public float bulletSpeed;
+
+        private void Start() {
+            
+        }
+
+        void Update() {
+            transform.position += transform.up * Time.deltaTime * bulletSpeed;
+        }
+
+        private void OnTriggerExit(Collider other) {
+            if (other.gameObject.CompareTag("OuterBox")) {
+                Destroy(gameObject);
+                Debug.Log("Cleaned bullet");
+            }
+        }
+    }
+}
--- Assets/UnityVrScripts/GunManger.cs
+++ Assets/UnityVrScripts/GunManger.cs
@@ -1,17 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace UnityVRScripts {
     public class GunManger : MonoBehaviour {
-        // Start is called before the first frame update
+        public GameObject bulletPrefab;
+        private XRGrabInteractable grabInteractable = null;
+
+        public float gunChargeRate;
+        public float gunMaxCharge;
+
+        public float _currentCharge;
+
+        private bool _triggerHeld = false;
         void Start() {
+            grabInteractable = GetComponent<XRGrabInteractable>();
+            grabInteractable.onActivate.AddListener(TriggerChange);
+            grabInteractable.onDeactivate.AddListener(TriggerChange);
+        }
+
+        private void OnDestroy() {
+            grabInteractable.onActivate.RemoveListener(TriggerChange);
+            grabInteractable.onDeactivate.RemoveListener(TriggerChange);
+        }
+
+        private void Update() {
+            if (_triggerHeld) {
+                _currentCharge += gunChargeRate * Time.deltaTime;
+                if (_currentCharge >= gunMaxCharge) {
+                    FireBullet();
+                    _currentCharge = 0.0f;
+                }
+            }
+            else if (_currentCharge>0) {
+                _currentCharge -= gunChargeRate * Time.deltaTime;
+            }
 
         }
 
-        // Update is called once per frame
-        void Update() {
+        void FireBullet() {
+            Instantiate(bulletPrefab, transform.position, transform.rotation * Quaternion.Euler(90, 0, 0));
+            Debug.Log("Fired Bullet");
+        }
 
+        void TriggerChange(XRBaseInteractor interactor) {
+            _triggerHeld = !_triggerHeld;
         }
     }
 }

38a6939425a1699beb8a61b4757fedb7c67a1453
Jasper <jasper@ngita.co.nz>
2020-07-19 15:38:01 +1200

    LightShit

--- Assets/UnityVrScripts/LightsOff.cs
+++ Assets/UnityVrScripts/LightsOff.cs
@@ -3,24 +3,23 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
-public class LightsOff : MonoBehaviour
-{
-    public bool LightsOn = false;
-    void Start () {
-        new Thread(() =>
-        {
-            Thread.Sleep(10000);
-            LightsOn = true;
-        }).Start();
-    }    
-    void Update ()
-    {
-        if (LightsOn)
-        {
-            GameObject[] allLights = GameObject.FindGameObjectsWithTag("TurnOff");
-            foreach (GameObject i in allLights)
-            {
-                i.SetActive(false);
+namespace UnityVRScripts {
+    public class LightsOff : MonoBehaviour {
+        public bool LightsOn = false;
+
+        void Start() {
+            new Thread(() => {
+                Thread.Sleep(10000);
+                LightsOn = true;
+            }).Start();
+        }
+
+        void Update() {
+            if (LightsOn) {
+                GameObject[] allLights = GameObject.FindGameObjectsWithTag("TurnOff");
+                foreach (GameObject i in allLights) {
+                    i.SetActive(false);
+                }
             }
         }
     }
